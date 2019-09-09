49ddf8d8532617dd822e36c1e510128b4d015746
salbertd <salbertd@umich.edu>
2019-09-09 15:36:53 -0400

    added helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+import random
+
+print("Devin")
+x = random.randint(0, 100)
+y = random.randint(0,100)
+print(x)
+print(y)
+sum = x + y
+print("Sum = ", sum)
+avg = sum / 2
+print("Average = ", avg)
