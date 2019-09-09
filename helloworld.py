18fab2d89c92cbb489bfce9d6088063b281241d1
salbertd <salbertd@umich.edu>
2019-09-09 15:39:36 -0400

    made prettier

--- helloworld.py
+++ helloworld.py
@@ -2,10 +2,10 @@ import random
 
 print("Devin")
 x = random.randint(0, 100)
-y = random.randint(0,100)
+y = random.randint(0, 100)
+sum = x + y
+avg = sum / 2
 print(x)
 print(y)
-sum = x + y
 print("Sum = ", sum)
-avg = sum / 2
 print("Average = ", avg)
